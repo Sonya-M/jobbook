21fb73a5a0c6f7259970fac1fc45a8a33aa4f911
Sonja Musicki <sonja.musicki@gmail.com>
2021-10-06 21:08:30 +0200

    added empty wiz-ctx

---
+++ src/store/wiz-context.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const WizContext = React.createContext({
+
+});
+
+export const WizContextProvider = (props) => {
+
+}
+
+export default WizContext;
