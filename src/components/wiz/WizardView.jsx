a5f20175588de9939ee53b7e37754e1ef3c7e879
Sonja Musicki <sonja.musicki@gmail.com>
2021-10-19 14:39:44 +0200

    added cancel btn to wizard

--- src/components/wiz/WizardView.jsx
+++ src/components/wiz/WizardView.jsx
@@ -1,5 +1,5 @@
-import styles from "./WizardView.module.css";
 import { Row, Col, ProgressBar } from "react-bootstrap";
+import { useHistory } from "react-router";
 import WizardSteps from "./WizardSteps";
 import WizSelect from "./WizSelect";
 import WizCandidateCard from "./WizCandidateCard";
@@ -11,18 +11,29 @@ import LoaderRipple from "../UI/LoaderRipple";
 import WizContext from "../../store/wiz-context";
 import { useContext } from "react";
 
+import { Button } from "react-bootstrap";
+import styles from "./WizardView.module.css";
+
 export default function WizardView() {
   const ctx = useContext(WizContext);
+  const history = useHistory();
+
+  const handleCancel = () => {
+    history.push("/admin");
+  };
 
   if (ctx.error) return <ErrorDisplay message={ctx.error} />;
   if (ctx.loading) return <LoaderRipple />;
 
   return (
-    <Row className="mt-4  m-0">
+    <Row className="m-0">
       <Col sm={3} lg={2} className={styles.stepsDiv}>
         <WizardSteps />
         <div className="text-center my-4" style={{ width: "100%" }}>
           <ProgressBar max="3" now={ctx.currentStep + 1} variant="dark" />
+          <Button size="sm" className={styles.cancelBtn} onClick={handleCancel}>
+            Cancel
+          </Button>
         </div>
         <WizSelectedInfo />
       </Col>
